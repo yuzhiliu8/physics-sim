53e8457cc7aeeb0a8670ce4376793966cc64f124
yuzhiliu8 <yuzhiliu8@gmail.com>
2025-12-29 16:27:26 -0500

    utils + sim testing

--- include/utils.hpp
+++ include/utils.hpp
@@ -9,6 +9,23 @@ namespace phys{
     inline constexpr double GRAVITY = 9.81;
 }
 
+namespace color {
+    inline float green[] = {0.25f, 0.61f, 0.04f, 1.0f};
+    inline float blue[] = {0.5f, 0.5f, 1.0f, 1.0f};
+}
+
+inline void add_triple(std::vector<float>& vec, float x, float y, float z){
+    vec.push_back(x);
+    vec.push_back(y);
+    vec.push_back(z);
+}
+
+inline void add_pair(std::vector<unsigned int>& vec, unsigned int x, unsigned int y){
+    vec.push_back(x);
+    vec.push_back(y);
+}
+
+
 struct Point {
     glm::vec3 pos;
     glm::vec3 last_pos;
